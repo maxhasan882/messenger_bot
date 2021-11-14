862a2dae743f44fa5897a5426f04080b3c2b60c6
hasan <razibul.hasan@brainstation-23.com>
2021-11-15 00:35:02 +0600

    init commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/test", func(c *gin.Context) {
+		log.Println("Inside Test")
+		c.JSON(200, gin.H{"Message": "This is test"})
+	})
+	r.GET("/webhook", func(c *gin.Context) {
+		log.Println("Inside Get")
+		c.JSON(200, []byte(c.Query("hub.challenge")))
+	})
+	r.POST("/webhook", func(c *gin.Context) {
+		jsonData, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			log.Println("Error: ", err)
+		}
+		log.Println("Response: ", jsonData)
+		c.JSON(200, "")
+	})
+	r.GET("/", func(c *gin.Context) {
+		log.Println("Inside Home")
+		c.JSON(200, gin.H{"Message": "This is Home"})
+	})
+	err := r.Run(":8008")
+	if err != nil {
+		return
+	}
+}
